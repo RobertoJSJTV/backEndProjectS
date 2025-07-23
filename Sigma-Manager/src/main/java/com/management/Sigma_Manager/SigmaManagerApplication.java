60642dd0fd2f9f17699904cf7b8bd0ed9e37d4c3
RobertoJSJTV <Robertojsj18@gmail.com>
2025-07-23 17:56:28 -0300

    adicao spring boot

---
+++ Sigma-Manager/src/main/java/com/management/Sigma_Manager/SigmaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.management.Sigma_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaManagerApplication.class, args);
+	}
+
+}
---
+++ Sigma-Manager/src/test/java/com/management/Sigma_Manager/SigmaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.Sigma_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
